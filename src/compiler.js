6f33759c2e82a4fcbc95aee10f701f0710e2ddb5
Fabian Irsara <info@fabian-irsara.com>
2012-04-10 19:42:11 +0200

    refine compiler

--- src/compiler.js
+++ src/compiler.js
@@ -5,16 +5,27 @@ var compiler = exports = module.exports = (function(self){
 
   self.compile = function(source, destination){
     var data = fs.readFileSync(source).toString();
+    var key;
 
-    for (var type in lex){
-      for (var key in lex[type]){
-        console.log(lex[type][key] + ' -> ' + key)
-        data = data.replace(new RegExp(lex[type][key], 'g'), key);
-      }
+    for (key in lex.tags){
+      data = data.replace(new RegExp(lex.tags[key] + "(\\s){", 'g'), key + ' {');
     }
 
+    for (key in lex.properties){
+      data = data.replace(new RegExp(lex.properties[key] + ':', 'g'), key + ':');
+      data = data.replace(new RegExp(lex.properties[key] + "(\\s):", 'g'), key + ':');
+    }
+
+    for (key in lex.values){
+      data = data.replace(new RegExp(lex.values[key] + ';', 'g'), key + ';');
+      data = data.replace(new RegExp(lex.values[key] + "(\\s);", 'g'), key + ';');
+      data = data.replace(new RegExp(lex.values[key] + "}", 'g'), key + '}');
+      data = data.replace(new RegExp(lex.values[key] + "\s}", 'g'), key + '}');
+    }
+
+    console.log(data);
     if (destination) fs.writeFileSync(destination, data);
-    
+
     return data;
   };
 
