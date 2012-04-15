668244a79d1c8c4506d4d589b2fa364fc17724fc
Fabian Irsara <info@fabian-irsara.com>
2012-04-15 12:16:13 +0200

    add @import support

--- src/sauerkraut.js
+++ src/sauerkraut.js
@@ -1,24 +1,52 @@
 // sauerkraut.js
 // Sauerkraut General API
 
-var compiler = require('./compiler')
+var SauerkrautCompiler = require('./compiler')
   , fs = require('fs');
+
+function trim(s)
+{
+  var l = 0; var r = s.length -1;
+  while(l < s.length && s[l] == ' ') { l++; }
+  while(r > l && s[r] == ' ') { r-=1; }
+  return s.substring(l, r+1);
+}
 
 function Sauerkraut(){}
 
-var fetchData = function(source){
-  var folder = source.substring(source.lastIndexOf('/'));
-  var data = fs.readFileSync(source).toString();
+Sauerkraut.prototype.fetchImport = function(sourceData){
+  var index = sourceData.indexOf('@import');
+  if (index === -1) return false;
 
-  // TODO: fetch @import directives
-  var imports = [];
+  var data = sourceData.substring(index);
+  var definition = data.substring(0, data.indexOf(';') + 1);
+  var importFile = definition.replace(/"/g, '');
+  importFile = importFile.replace(/'/g, '');
+  importFile = importFile.replace(/;/g, '');
+  importFile = importFile.replace(/@import/g, '');
+  importFile = trim(importFile);
+
+  return {file: importFile, definition: definition};
+};
+
+Sauerkraut.prototype.fetchData = function(source){
+  if (source.substring(source.length - 6) !== '.skcss') source = source + '.skcss';
+  var folder = source.substring(0, source.lastIndexOf('/') + 1)
+    , data = fs.readFileSync(source).toString()
+    , importFile;
+
+  while (importFile = this.fetchImport(data))
+  {
+    data = data.replace(importFile.definition, '');
+    data = this.fetchData(folder + importFile.file) + data;
+  }
 
   return data;
 };
 
 Sauerkraut.prototype.compile = function(source, destination){
-  var data = fetchData(source);
-  data = compiler.compile(data);
+  var data = this.fetchData(source);
+  data = SauerkrautCompiler.compile(data);
   if (destination) fs.writeFileSync(destination, data);
 };
 
