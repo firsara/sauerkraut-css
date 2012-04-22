1a5a6540d60865aed631d460d5b8d4648a640846
Fabian Irsara <info@fabian-irsara.com>
2012-04-22 18:34:31 +0200

    update lex, update compiler

--- src/compiler.js
+++ src/compiler.js
@@ -5,6 +5,8 @@ var SauerkrautLex = require('./lex');
 var SauerkrautCompiler = exports = module.exports = (function(self){
 
   var any = "([\\s\\S]*?)";
+  var word = "([\\S]*?)";
+  var space = "([\\s]*?)";
 
   self.compile = function(data){
     return compileWith(SauerkrautLex, data);
@@ -28,21 +30,21 @@ var SauerkrautCompiler = exports = module.exports = (function(self){
   var compileWith = function(lex, data){
     var key;
 
+    for (key in lex.properties){
+      data = data.replace(new RegExp("([\\s])" + lex.properties[key] + space + ':', 'gi'), '$1' + key + '$2:');
+    }
+
+    for (key in lex.values){
+      data = data.replace(new RegExp(lex.values[key] + any + ';', 'gi'), key + '$1;');
+      data = data.replace(new RegExp(lex.values[key] + any + '}', 'gi'), key + '$1}');
+    }
+
     for (key in lex.tags){
       data = data.replace(new RegExp(lex.tags[key] + any + "{", 'gi'), key + '$1{');
     }
 
     for (key in lex.pseudos){
       data = data.replace(new RegExp(lex.pseudos[key] + any + "{", 'gi'), key + '$1{');
-    }
-
-    for (key in lex.properties){
-      data = data.replace(new RegExp(lex.properties[key] + any + ':', 'gi'), key + '$1:');
-    }
-
-    for (key in lex.values){
-      data = data.replace(new RegExp(lex.values[key] + any + ';', 'gi'), key + '$1;');
-      data = data.replace(new RegExp(lex.values[key] + any + '}', 'gi'), key + '$1}');
     }
 
     return data;
