796acf5485fa067e7bb2191ed52226023e9185c9
Fabian Irsara <info@fabian-irsara.com>
2012-04-26 21:22:45 +0200

    update test

--- test/compile.js
+++ test/compile.js
@@ -1,11 +1,13 @@
 var fs = require('fs');
 var Sauerkraut = require('./../src/sauerkraut');
 var compiler = require('./../src/compiler');
+var jsdiff = require('./jsdiff');
 
 var kraut = new Sauerkraut();
 kraut.compile(__dirname+'/skcss/style.skcss', __dirname+'/skcss/style.compiled.css');
 kraut.inverse(__dirname+'/css/style.css', __dirname+'/css/style.compiled.skcss');
 
+fs.unlink(__dirname + '/diff.html');
 
 // Actual Test
 var source = fs.readFileSync(__dirname+'/css/style.css').toString();
@@ -19,4 +21,8 @@ if (reversed.replace(/\s/gi, '') === source.replace(/\s/gi, ''))
 else
 {
   console.log('Test failed');
+  console.log('See Log File (diff.html) for differences!');
+  var difference = jsdiff(reversed, source);
+  difference = '<!DOCTYPE HTML><html><head></head><body><pre>' + difference + '</pre></body>/</html>';
+  fs.writeFileSync(__dirname + '/diff.html', difference);
 }
