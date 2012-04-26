7e04d23bea8f553ec6812ef5fe1408b90e1ef6aa
Fabian Irsara <info@fabian-irsara.com>
2012-04-26 19:13:43 +0200

    update compiler (matches selectors better)

--- src/compiler.js
+++ src/compiler.js
@@ -27,26 +27,42 @@ var SauerkrautCompiler = exports = module.exports = (function(self){
     return compileWith(lex, data);
   };
 
+  var convert = function(key){
+    key = key.replace(/Ä/g, '_AE_');
+    key = key.replace(/ä/g, '_ae_');
+    key = key.replace(/Ü/g, '_UE_');
+    key = key.replace(/ü/g, '_ue_');
+    key = key.replace(/Ö/g, '_OE_');
+    key = key.replace(/ö/g, '_oe_');
+    key = key.replace(/ß/g, '_ss_');
+    key = key.replace(/@/g, '_at_');
+
+    return key;
+  };
+
   var compileWith = function(lex, data){
     var key;
 
+    data = '}' + data;
+    data = convert(data);
+
     for (key in lex.properties){
-      data = data.replace(new RegExp("([\\s])" + lex.properties[key] + space + ':', 'gi'), '$1' + key + '$2:');
+      data = data.replace(new RegExp("{" + any + convert(lex.properties[key]) + space + ':', 'gi'), '{$1' + key + '$2:');
     }
 
     for (key in lex.values){
-      data = data.replace(new RegExp(lex.values[key] + any + ';', 'gi'), key + '$1;');
+      data = data.replace(new RegExp(':' + any + convert(lex.values[key]) + any + ';', 'gi'), ':' + '$1' + key + '$2;');
     }
 
     for (key in lex.pseudos){
-      data = data.replace(new RegExp(lex.pseudos[key] + any + "{", 'gi'), key + '$1{');
+      data = data.replace(new RegExp('}' + any + "(\\b)" + '(' + convert(lex.pseudos[key]) + ')' + "(\\b)" + any + "{", 'gi'), '}' + '$1' + '$2' + key + '$4' + '$5{');
     }
 
     for (key in lex.tags){
-      data = data.replace(new RegExp("\\b" + lex.tags[key] + "\\b" + any + "{", 'gi'), key + '$1{');
+      data = data.replace(new RegExp('}' + any + "(\\b)" + '(' + convert(lex.tags[key]) + ')' + "(\\b)" + any + "{", 'gi'), '}' + '$1' + '$2' + key + '$4' + '$5{');
     }
 
-    return data;
+    return data.substring(1);
   };
 
   return self;
